80668295439c4c263ff519683a4299f7a537a5df
gbelda <gbelda56@gmail.com>
2021-10-21 15:20:00 +0200

    added alert message when bomb is clicked

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -30,7 +30,13 @@ if (userDifficulty == 1) {
 //difficoltà 2 => tra 1 e 81
 // difficoltà 3 => tra 1 e 49
 
+//Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 let bombs = []
+let safe = []
 for (let i = 1; i <= maxValue; i++) {
     const divEl = document.createElement('div')
     divEl.className = "cell"
@@ -38,33 +44,42 @@ for (let i = 1; i <= maxValue; i++) {
     divEl.append(i)
     container.append(divEl)
 
+    //I numeri nella lista delle bombe non possono essere duplicati.
     while (bombs.length < 16) {
         let bomb = getRandomNumber(1, maxValue)
         if (!bombs.includes(bomb)) {
             bombs.push(bomb)
+
         }
     }
-
+    //l'utente clicca su ogni cella:
     divEl.addEventListener('click', function () {
+
+        /* se il numero è presente nella lista dei numeri generati
+        la cella si colora di rosso e la partita termina*/
         if (bombs.includes(parseInt(divEl.innerHTML))) {
-            this.classList.add("red")
-            console.log();
+            let point = safe.length
+            alert(`GAME OVER hai fatto ${point} punti`)
+
+            for (let index = 0; index < maxValue; index++) {
+                bombs.includes(parseInt(divEl.innerHTML))
+                divEl.classList.add("red")
+            }
+
+
+            /* altrimenti la cella cliccata si colora di azzurro e 
+            l'utente può continuare a cliccare sulle altre celle */
         } else {
             this.classList.add("blue")
-
+            safe.push(this.innerHTML)
         }
+        console.log(safe);
     })
+
 }
 
 
 
 
-// })
-//Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
 
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-//I numeri nella lista delle bombe non possono essere duplicati.
 
